ea242b80de1765dbc8d574c2c84e19855b094321
Beatrice-Giacobbe <beatrice.giacobbe@it.ey.com>
2025-08-22 15:26:31 +0200

    add chat script

---
+++ Progetti/Giacobbe_chat.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+from dotenv import load_dotenv
+from openai import AzureOpenAI
+
+load_dotenv()
+client = AzureOpenAI(
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+)
+
+
+st.title("Chat simulata con API") 
+st.session_state["openai_model"] = os.getenv("AZURE_OPENAI_DEPLOYMENT")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
